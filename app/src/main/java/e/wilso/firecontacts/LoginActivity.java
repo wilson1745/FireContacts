424f2d7b799089805eb0a69763c7cdde5bf36523
wilson1745 <wilson-power0130@hotmail.com>
2018-06-03 17:27:43 +0100

    Firebase register system

--- app/src/main/java/e/wilso/firecontacts/LoginActivity.java
+++ app/src/main/java/e/wilso/firecontacts/LoginActivity.java
@@ -1,7 +1,9 @@
 package e.wilso.firecontacts;
 
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +11,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -69,13 +74,51 @@ public class LoginActivity extends AppCompatActivity {
    }
 
    public void login(View view) {
-      String email = ed_email.getText().toString();
-      String password = ed_password.getText().toString();
+      final String email = ed_email.getText().toString();
+      final String password = ed_password.getText().toString();
 
       SharedPreferences setting = getSharedPreferences("login", MODE_PRIVATE);
       setting.edit().putString("USER", email).apply();
 
       Log.d("AUTH", email + "/" + password);
-      auth.signInWithEmailAndPassword(email, password);
+      auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+         @Override
+         public void onComplete(@NonNull Task<AuthResult> task) {
+            if (!task.isSuccessful()) {
+               Log.d("onComplete", "登入失敗");
+               register(email, password);
+            }
+         }
+      });
+   }
+
+   private void register(final String email, final String password) {
+      new AlertDialog.Builder(LoginActivity.this)
+              .setTitle("登入問題")
+              .setMessage("無此帳號，是否要以此帳號與密碼註冊?")
+              .setPositiveButton("註冊",
+                      new DialogInterface.OnClickListener() {
+                         @Override
+                         public void onClick(DialogInterface dialog, int which) {
+                            createUser(email, password);
+                         }
+                      })
+              .setNeutralButton("取消", null)
+              .show();
+
+   }
+
+   private void createUser(String email, String password) {
+      auth.createUserWithEmailAndPassword(email, password)
+              .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                         @Override
+                         public void onComplete(@NonNull Task<AuthResult> task) {
+                            String message = task.isComplete() ? "註冊成功" : "註冊失敗";
+                            new AlertDialog.Builder(LoginActivity.this)
+                                    .setMessage(message)
+                                    .setPositiveButton("OK", null)
+                                    .show();
+                         }
+                      });
    }
 }
